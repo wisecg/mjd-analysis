4e856c729df60ac8e1bf728cad5ead2103d7fcfc
Clint Wiseman <wisecg.neontetra@gmail.com>
2016-09-28 10:33:43 -0400

    near term data quality checker

---
+++ data-quality/data-quality.cc
@@ -0,0 +1,256 @@
+// data-quality.cc
+// C. Wiseman, 9/10/2016
+// (Adapted from splice_waveforms.cc by T. Caldwell)
+//
+// Created to check problems with the "decimation" method
+// used in splice_waveforms.
+//
+// Result:  The decimation seems to be done incorrectly.
+//          It doesn't remove the same samples per waveform,
+//          so values like "max ADC" seem to decrease over time.
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+
+#include "TChain.h"
+#include "TFile.h"
+#include "TGraph.h"
+
+#include "MJTRun.hh"
+#include "MGTEvent.hh"
+#include "GATDataSet.hh"
+
+using namespace std;
+
+void halve_array(vector<double>& v, bool ave)
+{
+	for(unsigned i=0; i<v.size(); i+=2)
+	{
+		if(ave) v[i/2] = (v[i] + v[i+1]) / 2;
+		else v[i/2] = v[i];
+	}
+	v.resize(v.size() / 2);
+}
+
+TGraph* graph(string name, vector<double>& x, vector<double>& y, string sx, string sy, int color)
+{
+	if(x.size() == 0 || x.size() != y.size())
+	return NULL;
+	TGraph* g = new TGraph(x.size(), &x.front(), &y.front());
+	g->SetName(name.c_str());
+	g->SetTitle(("#font[132]{" + name + "}").c_str());
+	g->GetXaxis()->SetTitle(("#font[132]{" + sx + "}").c_str());
+	g->GetYaxis()->SetTitle(("#font[132]{" + sy + "}").c_str());
+	g->GetXaxis()->SetLabelFont(132);
+	g->GetYaxis()->SetTitleFont(132);
+	g->SetMarkerStyle(20);
+	g->SetMarkerSize(0.2);
+	g->SetLineColor(color);
+	g->SetMarkerColor(color);
+	return g;
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2){
+		cout << "Usage:\n ./data-quality [run number] (Optional: [last run] [first event] [last event]) \n\n";
+		return 1;
+	}
+	int fRun = stoi(argv[1]);
+	int lRun=0, fEvent=0, lEvent=0;
+	if (argc > 2) lRun = stoi(argv[2]);
+	if (argc > 4) {
+		fEvent = stoi(argv[3]);
+		lEvent = stoi(argv[4]);
+	}
+	printf("\n\ninputs: %i  %i  %i  %i\n",fRun,lRun,fEvent,lEvent);
+
+	GATDataSet ds;
+	if (lRun == 0) ds.AddRunNumber(fRun);
+	else if (lRun != 0) ds.AddRunRange(fRun,lRun);
+
+	// NOTE: analysis machine version may only have access to built data,
+	//       PDSF version will have both built & reconstructed.
+	TChain *built = ds.GetBuiltChain(false); // false: don't friend the recon chain
+	// TChain *recon = ds.GetGatifiedChain();
+	printf("Found %lli entries.\n",built->GetEntries());
+	if (lEvent==0) lEvent = (int)built->GetEntries();
+
+	MGTEvent *event = new MGTEvent();
+	MJTRun *mjRun = new MJTRun();
+    built->SetBranchAddress("event", &event);
+	built->SetBranchAddress("run",&mjRun);
+
+	MJTChannelMap *map = ds.GetChannelMap();
+	MJTChannelSettings *set = ds.GetChannelSettings();
+	vector<uint32_t> enab = set->GetEnabledIDList(); // gets ID's for the first run only
+
+	const int ndet = map->NumberOfRows();	// all possible detectors in channel map
+	std::map<int, int> index_map;  // map the channel number to channel map column number (ndet)
+	for(int i=1; i<2000; i++)
+		if(map->GetInt(i, "kIDHi") == i)
+			index_map[i] = map->GetInt(i, "kSegmentNumber");
+	// for (std::map<int,int>::iterator it = index_map.begin(); it != index_map.end(); ++it)
+    	// cout << it->first << " , " << it->second << "\n";
+
+	const int nbase = 200;	// number of baseline samples per waveform
+	const unsigned max_array_size = 2 << 16;
+
+	vector< vector<double> > t(ndet);
+    vector< vector<double> > v(ndet);
+    vector< vector<double> > vbase(ndet);
+	vector<int> wf_prescale(ndet, 1);
+
+	vector< vector<double> > t1(ndet);
+	vector< vector<double> > vmaxima(ndet);
+	vector< vector<double> > tdecimate(ndet);
+	vector< vector<double> > vdecimate(ndet);
+
+	// Figure out time offsets s/t the first event requested is at t = 0.
+	double localTime = 0; 	// 1. time since first event in this run ("local run time")
+	double chainTime = 0;	// 2. time since first event in the chain ("global run time")
+	double unixTime = 0;	// 3. absolute unix time of event
+	double timeSinceFirst = 0;  // 4. time since first event specified ("t=0 time")
+	built->GetEntry(0);
+	int iRun = mjRun->GetRunNumber();
+	double chainRunStart = mjRun->GetStartTime();
+	double chainEventOffset = event->GetTime()/1e9;
+	double thisRunStart = chainRunStart;
+	double thisEventOffset = chainEventOffset;
+	built->GetEntry(fEvent);
+	double firstRunStart = mjRun->GetStartTime();
+	double firstEventOffset = event->GetTime()/1e9;
+	double firstChainTime = (firstRunStart-chainRunStart)+firstEventOffset-chainEventOffset;
+
+	for(int iev = fEvent; iev < lEvent; iev++)
+	{
+		if(iev % 10000 == 0) printf("%i\t%i\n", iev, (int) built->GetEntries());
+		if(iev >= built->GetEntries()) { cout << "user's event range is beyond the end of the run!\n"; break; }
+		built->GetEntry(iev);
+
+		if (mjRun->GetRunNumber() != iRun){
+			thisRunStart = mjRun->GetStartTime();
+			thisEventOffset = event->GetTime()/1e9;
+			iRun = mjRun->GetRunNumber();
+		}
+		localTime = event->GetTime()/1e9 - thisEventOffset;
+		chainTime = (thisRunStart - chainRunStart) + localTime;
+		unixTime = thisRunStart + localTime;
+		timeSinceFirst = chainTime - firstChainTime;
+		// printf("loc %.2f  chain %.2f  unix %.0f  tsf %.2f\n", localTime ,chainTime, unixTime, timeSinceFirst);
+		double time = timeSinceFirst;
+
+		for(int ich=0; ich<(int)event->GetNWaveforms(); ich++)
+		{
+			MGTWaveform *wf = event->GetWaveform(ich);
+			int ch_id = wf->GetID();
+			if (ch_id%2==1) continue;	// skip LG channels
+			int ch_index = index_map[ch_id];  // detector number
+
+			// get string and detector positions from channel map and ignore pulser tag channels
+			string strName = map->GetString(ch_id,"kStringName");
+			string detName = map->GetString(ch_id,"kDetectorName");
+			char pos = strName.back();
+			int strNum = pos - '0';
+			int det = map->GetInt(ch_id,"kDetectorPosition"); // if det = 0, pulser tag channel
+			string detector = strName+"D"+to_string(det);
+			if (det == 0) continue;
+			// printf("ch_index %i  chan %i  detector %s  detName %s\n",ch_index,ch_id,detector.c_str(),detName.c_str());
+
+			TH1D* h = wf->GimmeHist(detector.c_str());
+
+			for(int i=0; i<4; i++) // replace last 4 samples with 4 next-to-last
+				h->SetBinContent(h->GetNbinsX()-i, h->GetBinContent(h->GetNbinsX()-4));
+
+			double offset = wf->GetTOffset()/1e9; // time offset (in sec) of waveforms that are grouped into the same event
+			double base = 0.0;
+			double rms = 0.0;
+			for(int i=4; i<nbase; i++) base += h->GetBinContent(i);
+			base /= nbase;
+			for(int i=4; i<nbase; i++) rms += pow(h->GetBinContent(i) - base, 2);
+			rms = sqrt(rms) / base;
+
+			// max and min values of waveform (useful to have both when we trigger on pos/neg pulses)
+			double wmax = -1.0e6;
+			double wmin =  1.0e6;
+			bool rail = false;
+
+			// looping over the waveform
+			for(int i = 1; i <= h->GetNbinsX(); i += wf_prescale[ch_index])
+			{
+				double wfTime = time + offset + (h->GetBinCenter(i))/1e9;
+
+				t[ch_index].push_back(wfTime);
+				v[ch_index].push_back(h->GetBinContent(i));
+
+				if(t[ch_index].size() == max_array_size)
+				{
+					halve_array(t[ch_index], false);
+					halve_array(v[ch_index], false);
+					wf_prescale[ch_index] *= 2;
+					tdecimate[ch_index].push_back(wfTime);
+					vdecimate[ch_index].push_back(1);
+					printf("halving arrays, detector %s.  prescale now %i\n",detector.c_str(),wf_prescale[ch_index]);
+				}
+				wmax = std::max(h->GetBinContent(i), wmax);
+				wmin = std::min(h->GetBinContent(i), wmin);
+			}
+			delete h;
+
+			// this gets the max value found during sampling, not the true max/min value, but maybe close enough.
+			t1[ch_index].push_back(time);
+			if(TMath::Abs(wmax) > TMath::Abs(wmin))
+				vmaxima[ch_index].push_back(wmax);
+			else
+				vmaxima[ch_index].push_back(wmin);
+		}
+	}
+
+
+	// ==================== OUTPUT ====================
+	string runs = to_string(fRun);
+	if (lRun != 0) runs += "_" + to_string(lRun);
+	string fname = "data-quality_" + runs;
+	TFile* outfile = new TFile((fname + ".root").c_str(), "recreate");
+    outfile->cd();
+
+	for(int i = 0; i < ndet; i++)
+	{
+		// get detector position and name for plots
+		int dpos = 0;
+		string detName = "";
+		for (std::map<int,int>::iterator it2 = index_map.begin(); it2 != index_map.end(); ++it2)
+		{
+			if (it2->second == i) {
+				int ch_id = it2->first;
+				string strName = map->GetString(ch_id,"kStringName");
+				char pos = strName.back();
+				int strNum = pos - '0';
+				dpos = map->GetInt(ch_id,"kDetectorPosition");
+				detName = strName+"D"+to_string(dpos);
+				break;
+			}
+		}
+		printf("detector %s  size %lu\n",detName.c_str(),t[i].size());
+		if(t[i].size() == 0) continue;
+
+		int color = (dpos + 1) * 2;
+		if(dpos > 3) color = 1;
+
+		TGraph *gwf = graph(detName + "_gwf", t[i], v[i],"Time (sec)", "ADC", color);
+		gwf->Write();
+
+		TGraph *gmax = graph(detName + "_gmax", t1[i], vmaxima[i], "Time (sec)", "ADC", color);
+		gmax->Write();
+
+		TGraph *decTimes = graph(detName + "_dec", tdecimate[i], vdecimate[i], "Time (sec)", "ADC", color);
+		decTimes->SetMarkerStyle(kFullDotLarge);
+		decTimes->SetMarkerColor(kRed);
+		decTimes->Write();
+
+	}
+	outfile->Close();
+    return 1;
+}
---
+++ data-quality/data-report.py
@@ -0,0 +1,160 @@
+#!/usr/local/bin/python
+
+import sys, os
+from pylatex import *
+from ROOT import *
+import numpy as np
+
+M1Channels = {"P1D1":584, "P1D2":582, "P1D3":580, "P1D4":578, "P2D1":692, "P2D2":648, "P2D3":640, "P2D4":642, "P3D1":616, "P3D2":610, "P3D3":608, "P3D4":664, "P4D1":624, "P4D2":628, "P4D3":688, "P4D4":694, "P4D5":614, "P5D1":680, "P5D2":678, "P5D3":672, "P5D4":696, "P6D1":632, "P6D2":630, "P6D3":626, "P6D4":690, "P7D1":600, "P7D2":598, "P7D3":594, "P7D4":592}
+
+M2Channels = {"P1D1":1140, "P1D2":1142, "P1D3":1110, "P1D4":1204, "P2D1":1174, "P2D2":1144, "P2D3":1106, "P2D4":1108, "P2D5":1138, "P3D1":1176, "P3D2":1172, "P3D3":1202, "P4D1":1170, "P4D2":1208, "P4D3":1206, "P4D4":1136, "P4D5":1168, "P5D1":1330, "P5D2":1304, "P5D3":1332, "P5D4":1302, "P6D1":1296, "P6D2":1298, "P6D3":1328, "P6D4":1234, "P7D1":1268, "P7D2":1238, "P7D3":1236, "P7D4":1232}
+
+def main(argv):
+	if os.path.isfile(argv[0]):
+		infile = TFile(argv[0])
+	else:
+		print "File",argv[0],"doesn't exist! Exiting ..."
+		return
+
+	plotTypes = ["gwf","gbase","grms","hbase","hrms","spec"]
+
+	print "Creating plots ..."
+	plotList = createPlots(infile)
+
+	print "Populating document ..."
+	doc = Document('data-report')
+	fillDocument(doc, plotList, M2Channels, plotTypes)
+
+	print "Compiling document ..."
+	doc.generate_pdf('data-report', clean_tex=True)
+
+	print "Cleaning up plots..."
+	for plot in plotList:
+		os.remove(plot)
+
+
+def createPlots(infile):
+
+	# don't display "created canvas messages"
+	gROOT.SetBatch(kTRUE)
+	gROOT.ProcessLine( "gErrorIgnoreLevel = 2001;")
+
+	plotList = []
+	for key in gDirectory.GetListOfKeys():
+		name = key.GetName()
+		obj = key.ReadObj()
+		plotname = "%s.png" % name
+		plotList.append(plotname)
+
+		if "gwf" in name or "gbase" in name:
+			can = TCanvas("can","Bob Ross's Canvas",1600,550)
+			obj.SetMarkerStyle(kFullDotMedium)
+			obj.Draw("AP")
+			can.Print(plotname)
+		elif "spec" in name:
+			can = TCanvas("can","Bob Ross's Canvas",750,450)
+			can.SetLogy(1)
+			obj.Draw()
+			can.Print(plotname)
+		elif "grms" in name:
+			can = TCanvas("can","Bob Ross's Canvas",750,450)
+			obj.SetMarkerStyle(kFullDotMedium)
+			obj.Draw("AP")
+			can.Print(plotname)
+		else :
+			can = TCanvas("can","Bob Ross's Canvas",750,450)
+			obj.Draw()
+			can.Print(plotname)
+
+	return plotList
+
+
+def fillDocument(doc, plotList, detList, plotTypes):
+
+	# ======= preamble =======
+	doc.preamble.append(NoEscape("""
+	\usepackage[margin=1in]{geometry}
+	\usepackage{fancyhdr}
+	\usepackage{bold-extra}
+	\usepackage{datetime}
+	\usepackage{hyperref}
+	\usepackage{graphicx}
+	\usepackage[]{algorithm2e}
+	\usepackage{framed}
+	\usepackage{enumitem}
+	\usepackage{multicol} % 2-col table of contents
+	\\newcommand{\\tty}{\\texttt}
+	\\newcommand{\\ita}{\\textit}
+	\\newcommand{\\bol}{\\textbf}
+	\\newcommand{\\smc}{\\textsc}
+	\\newenvironment{description_nospace}
+	{ \\begin{description}
+	    \setlength{\itemsep}{0pt}
+	    \setlength{\parskip}{0pt}
+	    \setlength{\parsep}{0pt}     }
+	{ \end{description}                  }
+	\\newenvironment{enumerate_nospace}
+	{ \\begin{enumerate}
+	    \setlength{\itemsep}{0pt}
+	    \setlength{\parskip}{0pt}
+	    \setlength{\parsep}{0pt}     }
+	{ \end{enumerate}                  }
+	"""))
+
+	# ======= begin docment body =======
+	doc.append(NoEscape("""
+	\\pagestyle{fancy}
+	\\fancyhf{}
+	\\rfoot{\\thepage}
+	\\begin{center}
+	\Large\\bol{Majorana Data Quality Report}\\\\\\
+	\large\ita{Auto-generated: \\today, \\hhmmsstime}
+	\end{center}
+	\\begin{centering}
+	  \\tableofcontents
+	\\end{centering}
+	\\newpage
+	\\lhead{\\ita{MJD Data Quality Report}}
+	\\lfoot{\\ita{RS \& DC Group}}
+	\\renewcommand{\\footrulewidth}{0.4pt}
+	\\rfoot{\\thepage}
+	\\renewenvironment{framed}[1][\hsize]
+	  {\MakeFramed{\hsize#1\advance\hsize-\width \FrameRestore}}%
+	  {\endMakeFramed}
+	"""))
+
+	dets = sorted(detList.keys())
+	for det in dets:
+		doc.append(NoEscape("\\newpage"))
+		with doc.create(Section(det)):
+			doc.append('')
+
+		doc.append(NoEscape("\\begin{figure}[!ht]"))
+		doc.append(NoEscape("\\centering"))
+
+		# do the plots in the given order
+		# plotTypes = ["gwf","gbase","grms","hbase","hrms","spec"]
+		for plType in plotTypes:
+			for filename in plotList:
+				plot = ""
+				if filename.endswith('.png'):
+					plot = filename[:-4]
+				if plType in plot and det in plot:
+					if plType == "gwf":
+						# doc.append(NoEscape("\\includegraphics[width=500pt]{%s}" % plot))
+						doc.append(NoEscape("\hspace*{-0.7cm}\includegraphics[width=510pt]{%s}" % plot))
+					if plType == "gbase":
+						doc.append(NoEscape("\hspace*{-0.7cm}\includegraphics[width=510pt]{%s}" % plot))
+					if plType == "grms":
+						doc.append(NoEscape("\\includegraphics[width=225pt]{%s}" % plot))
+					if plType == "hbase":
+						doc.append(NoEscape("\\includegraphics[width=225pt]{%s}" % plot))
+					if plType == "hrms":
+						doc.append(NoEscape("\\includegraphics[width=225pt]{%s}" % plot))
+					if plType == "spec":
+						doc.append(NoEscape("\\includegraphics[width=225pt]{%s}" % plot))
+
+		doc.append(NoEscape("\\end{figure}"))
+
+if __name__ == '__main__':
+	main(sys.argv[1:])
